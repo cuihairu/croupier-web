489b98f57a31661cc8b2bea590d20aca474bf98f
cuihairu <chuihairu@gmail.com>
2025-10-24 06:53:42 +0800

    chore(branding): 将标题与欢迎文案从 'Ant Design Pro' 更新为 'Croupier'；登录页标题同步

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -159,7 +159,7 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
+          title="Croupier"
           subTitle={intl.formatMessage({ id: 'pages.layouts.userLayout.title' })}
           initialValues={{
             autoLogin: true,
